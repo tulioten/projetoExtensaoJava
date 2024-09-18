ec84215107ff0f99d6f4ed6fd7c8e8119c37b30c
tulioten <tuliotenorio0@gmail.com>
2024-09-17 22:33:09 -0300

    corrigida a quantidade total, antes somava tudo independente de qual fosse o produto.

--- Estoque.java
+++ Estoque.java
@@ -21,30 +21,33 @@ public class Estoque {
     public void imprimirEstoque() {
         for (Entry<String, Map<String, Produto>> entry : produtos.entrySet()) {
             String nome = entry.getKey();
-            System.out.println("Produto: " + nome);
+            System.out.println("\nProduto: " + nome + "\n");
 
             for (Entry<String, Produto> tamanhoEntry : entry.getValue().entrySet()) {
                 Produto produto = tamanhoEntry.getValue();
-                System.out.println("Tamanho: " + produto.getTamanho() + 
+                System.out.println(" Tamanho: " + produto.getTamanho() + 
                                     "\n Categoria: " + produto.getCategoria() +
                                     "\n Gênero: " + produto.getGenero() +
                                     "\n Modelagem: " + produto.getModelagem() +
-                                    "\n Quantidade: " + produto.getQuantidade()
+                                    "\n Quantidade: " + produto.getQuantidade() + "\n"
                 );
             } 
         }
     }
 
-    public int quantidadeTotalProduto() {
-        int total = 0;
+    public Map<String, Integer> quantidadeTotalPorProduto() {
+        Map<String, Integer> totalPorProduto = new HashMap<>();
 
         for (Entry<String, Map<String, Produto>> entry : produtos.entrySet()) {
-            Map<String, Produto> tamanhos = entry.getValue();
-            for (Produto produto : tamanhos.values()) {
+            String nomeProduto = entry.getKey();
+            int total = 0;
+
+            for (Produto produto : entry.getValue().values()) {
                 total += produto.getQuantidade();
             }
+            totalPorProduto.put(nomeProduto, total);
         }
-        return total;
+        return totalPorProduto;
     }
 
     public static void main(String[] args) {
@@ -52,14 +55,14 @@ public class Estoque {
 
         estoque.adicionarProduto(new Produto("Maiô", "P","Kit Natação", "Fem", "Infantil", 50));
         estoque.adicionarProduto(new Produto("Maiô", "G", "Kit Natação", "Fem", "Adulto", 12));
-        
+
         estoque.adicionarProduto(new Produto("Sunga", "M", "Kit Natação", "Masc", "Adulto", 9));
         estoque.adicionarProduto(new Produto("Sunga", "GG", "Kit Natação", "Masc", "Infantil", 34));
 
         estoque.imprimirEstoque();
 
-        int total = estoque.quantidadeTotalProduto();
+        Map<String, Integer> totais = estoque.quantidadeTotalPorProduto();
+        System.out.println(totais);
 
-        System.out.println("A quantidade total do produto é: " + total+ " unidades.");
     }
 }
