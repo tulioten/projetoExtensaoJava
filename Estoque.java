06ccd7c7c7148bc61ab2003a565e4f5a0ea1a7a9
tulioten <tuliotenorio0@gmail.com>
2024-09-24 00:19:23 -0300

    added modify quantity and print total.

--- Estoque.java
+++ Estoque.java
@@ -16,49 +16,64 @@ public class Estoque {
         List<Tamanho> tamanho = produto.verificarQuantidade();
         
         produtos.putIfAbsent(nome, produto);
-
-        // System.out.println(produto);
-        // System.out.println(tamanho);
-        // System.out.println(tipo);
-        // produtos.get(nome).put( produto);
     }
 
-    public void imprimirEstoque() {
+    public void modificarQuantidade(String item, String tam, int quant) {
+        for (Entry<String, Produto> entry : produtos.entrySet()) {
+            if (entry.getKey().equalsIgnoreCase(item)) {
+                Produto produto = entry.getValue();
+                produto.alterarQuantidade(tam, quant);
+            }
+        }
+    }
+
+    public void imprimirEstoqueTotal() {
         for (Entry<String, Produto> entry : produtos.entrySet()) {
             String nome = entry.getKey();
             Produto produto = entry.getValue(); 
             System.out.println(entry.getKey());
             System.out.println(produto.verificarQuantidade());
-            // List<Tamanho> tamanhos = entry.getValue()
-            // System.out.println("\nProduto: " + nome  );
-            // System.out.println();
         }
     }
 
-    public void quantidadeTotalPorProduto(Produto produto){}
+    public void imprimirEstoqueDoProduto(String item) {
+        for (Entry<String, Produto> entry : produtos.entrySet()) {
+            if (entry.getKey().equalsIgnoreCase(item)) {
+                System.out.println(entry);
+            }
+        }
+    }
 
-    public void quantidadeTotalPorProduto() {
-        Map<String, Integer> totalPorProduto = new HashMap<>();
+    public void quantidadeTotalPorProduto(String item){
+        for (Entry<String, Produto> entry : produtos.entrySet()) {
+            if (entry.getKey().equalsIgnoreCase(item)) {
+                
+                Produto produto = entry.getValue();
+                List<Tamanho>tamanhosPorProduto = produto.verificarQuantidade();
+                
+                int total = 0;
+                for (Tamanho t : tamanhosPorProduto) {
+                    total += t.getQuantidade();
+            }
+                System.out.println(item + " têm " + total + " unidades.");
+            }
+        }
+        
+    }
 
+    public void quantidadeTotalEstoque() {
         for (Entry<String, Produto> entry : produtos.entrySet()) {
             System.out.println(entry.getValue());
             Produto produto = entry.getValue();
+            
 
             List<Tamanho>tamanhosPorProduto = produto.verificarQuantidade();
             int total = 0;
             for (Tamanho t : tamanhosPorProduto) {
                 total += t.getQuantidade();
             }
-            System.out.println(entry.getValue() + " " + total);
-            // String nomeProduto = entry.getKey();
-            // int total = 0;
-
-            // for (Produto produto : entry.getValue().values()) {
-            //     ;
-            // }
-            // totalPorProduto.put(nomeProduto, total);
+            System.out.println(produto.getNome() + " têm " + total + " peças");
         }
-        // return totalPorProduto;
     }
 
     public static void main(String[] args) {
@@ -67,13 +82,14 @@ public class Estoque {
         estoque.adicionarProduto(new Produto("Sunga", "Kit natação", "uniforme"));
         estoque.adicionarProduto(new Produto("Maiô", "Kit natação", "uniforme"));
         estoque.adicionarProduto(new Produto("Blusa M/M", "Kit Educando", "uniforme"));
-        // estoque.imprimirEstoque();
-        estoque.quantidadeTotalPorProduto();
-
+        // estoque.imprimirEstoqueTotal();
+        // estoque.quantidadeTotalPorProduto();
+        estoque.imprimirEstoqueDoProduto("Sunga");
+        estoque.modificarQuantidade("Sunga", "p_adulto", 60);
+        estoque.imprimirEstoqueDoProduto("Sunga");
         // Map<String, Integer> totais = estoque.quantidadeTotalPorProduto();
         // System.out.println(totais);
 
     }
 }
-
 
