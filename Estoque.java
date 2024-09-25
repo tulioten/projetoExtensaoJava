e02db70c74154efc54e3e2a6b39ac17488897d7c
tulioten <tuliotenorio0@gmail.com>
2024-09-25 15:10:58 -0300

    reduced quantity of loops

--- Estoque.java
+++ Estoque.java
@@ -9,24 +9,7 @@ public class Estoque {
     public Estoque() {
         produtos = new HashMap<>();
     }
-
-    public void adicionarProduto(Produto produto) {
-        String nome = produto.getNome();
-        String tipo = produto.getTipo();
-        List<Tamanho> tamanho = produto.verificarQuantidade();
-        
-        produtos.putIfAbsent(nome, produto);
-    }
-
-    public void modificarQuantidade(String item, String tam, int quant) {
-        for (Entry<String, Produto> entry : produtos.entrySet()) {
-            if (entry.getKey().equalsIgnoreCase(item)) {
-                Produto produto = entry.getValue();
-                produto.alterarQuantidade(tam, quant);
-            }
-        }
-    }
-
+    
     public void imprimirEstoqueTotal() {
         for (Entry<String, Produto> entry : produtos.entrySet()) {
             String nome = entry.getKey();
@@ -34,31 +17,6 @@ public class Estoque {
             System.out.println(entry.getKey());
             System.out.println(produto.verificarQuantidade());
         }
-    }
-
-    public void imprimirEstoqueDoProduto(String item) {
-        for (Entry<String, Produto> entry : produtos.entrySet()) {
-            if (entry.getKey().equalsIgnoreCase(item)) {
-                System.out.println(entry);
-            }
-        }
-    }
-
-    public void quantidadeTotalPorProduto(String item){
-        for (Entry<String, Produto> entry : produtos.entrySet()) {
-            if (entry.getKey().equalsIgnoreCase(item)) {
-                
-                Produto produto = entry.getValue();
-                List<Tamanho>tamanhosPorProduto = produto.verificarQuantidade();
-                
-                int total = 0;
-                for (Tamanho t : tamanhosPorProduto) {
-                    total += t.getQuantidade();
-            }
-                System.out.println(item + " têm " + total + " unidades.");
-            }
-        }
-        
     }
 
     public void quantidadeTotalEstoque() {
@@ -76,6 +34,35 @@ public class Estoque {
         }
     }
 
+    public void adicionarProduto(Produto produto) {
+        String nome = produto.getNome();
+        String tipo = produto.getTipo();
+        List<Tamanho> tamanho = produto.verificarQuantidade();
+        
+        produtos.putIfAbsent(nome, produto);
+    }
+
+    public void modificarQuantidade(String item, String tam, int quant) {
+            Produto produto = produtos.get(item);
+            produto.alterarQuantidade(tam, quant);
+        }
+    
+    public void imprimirEstoqueDoProduto(String item) {
+        Produto produto = produtos.get(item);
+        System.out.println(produto);   
+        }
+
+    public void quantidadeTotalPorProduto(String item){
+        Produto produto = produtos.get(item);
+        List<Tamanho>tamanhosPorProduto = produto.verificarQuantidade();
+        int total = 0;
+        for (Tamanho t : tamanhosPorProduto) {
+            total += t.getQuantidade();
+        }
+        System.out.println(item + " têm " + total + " unidades.");
+    }
+        
+
     public static void main(String[] args) {
         Estoque estoque = new Estoque();
 
@@ -83,10 +70,10 @@ public class Estoque {
         estoque.adicionarProduto(new Produto("Maiô", "Kit natação", "uniforme"));
         estoque.adicionarProduto(new Produto("Blusa M/M", "Kit Educando", "uniforme"));
         // estoque.imprimirEstoqueTotal();
-        // estoque.quantidadeTotalPorProduto();
-        estoque.imprimirEstoqueDoProduto("Sunga");
+        // estoque.imprimirEstoqueDoProduto("Sunga");
         estoque.modificarQuantidade("Sunga", "p_adulto", 60);
-        estoque.imprimirEstoqueDoProduto("Sunga");
+        // estoque.imprimirEstoqueDoProduto("Sunga");
+        estoque.quantidadeTotalPorProduto("Sunga");
         // Map<String, Integer> totais = estoque.quantidadeTotalPorProduto();
         // System.out.println(totais);
 
